95d613d8a2cc21dfe9b57a4892d12912084c29ae
nikita_zhmurko <n.zhmurko27@gmail.com>
2021-09-29 12:11:07 +0300

    env + Post model

--- apps/social_network/models.py
+++ apps/social_network/models.py
@@ -1,11 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
 
     class Meta:
-        ordering = ["-name"]
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return self.name
+        return self.title
--- core/settings.py
+++ core/settings.py
@@ -9,23 +9,31 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, "config/.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-xl+l63b44dmy3r7bvvl3_^_ckh+9sbnsdnfh0h&gltwunalwe9"
-)
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -80,8 +88,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASS"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
@@ -146,3 +158,12 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .setting_local import *
+except ImportError as e:
+    print(e)
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
